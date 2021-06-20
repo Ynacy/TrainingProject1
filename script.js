7cacd7ef3b5983e4f63e563e4683368c7edd9910
Ivan <ivan-sokolov-2004@list.ru>
2021-06-20 10:50:45 +0600

    ProjectCards

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert('hi')
+const slides = document.querySelectorAll('.slide')
+for (const slide of slides) {
+  slide.addEventListener('click', () => {
+    clearActiveClasses()
+
+    slide.classList.add('active')
+  })
+}
+function clearActiveClasses() {
+  slides.forEach((slide) => {
+    slide.classList.remove('active')
+  })
+}
